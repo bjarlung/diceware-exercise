bd6b52bad81bd537918977265c330d2bc15b4f15
Beatrice Järlung <beatrice.jarlung@evry.com>
2020-04-22 17:11:23 +0200

    WIP Started on dice-ware implementation

--- src/main/kotlin/se/vbgt/diceware/Application.kt
+++ src/main/kotlin/se/vbgt/diceware/Application.kt
@@ -1,5 +1,21 @@
 package se.vbgt.diceware
 
+import se.vbgt.diceware.DiceWareUtil.mapWords
+
 fun main() {
-    TODO("Implement me!")
+
+    fun getRollsForOneWord(): List<Int> = (1..5).map { Dice.rollDice() }
+
+    val diceResultList: List<List<Int>> = (1..6).map { getRollsForOneWord() }
+
+    diceResultList.forEachIndexed{index, it -> println("Index: $index. Value: $it")}
+
+    val wordMap: Map<List<Int>, String> = mapWords(*diceResultList.toTypedArray())
+
+    println(wordMap)
+
+    println(
+        mapWords()
+    )
+
 }
--- src/main/kotlin/se/vbgt/diceware/DiceWareUtil.kt
+++ src/main/kotlin/se/vbgt/diceware/DiceWareUtil.kt
@@ -1,6 +1,22 @@
 package se.vbgt.diceware
 
 object DiceWareUtil {
-    fun mapWords(vararg wordRoll: List<Int>): Map<List<Int>, String> = TODO()
+    fun mapWords(vararg wordRoll: List<Int>): Map<List<Int>, String> {
+        require(wordRoll.isNotEmpty()) {"No params"}
+        var wordMap = mutableMapOf<List<Int>, String>()
+
+        wordRoll.forEach { wordMap.put(it, mapWord(it)) }
+
+        return wordMap
+    }
     fun randomLetter(horizontalDice: Int, verticalDice: Int): Char = TODO()
+
+    private fun mapWord(wordRoll: List<Int>): String {
+        require(wordRoll.size == 5 ) {
+            "Size of word roll not valid"
+        }
+        // TODO Reader for file
+        // TODO Find word
+        return "wordFromFile"
+    }
 }
